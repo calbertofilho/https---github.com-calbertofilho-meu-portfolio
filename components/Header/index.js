d7223c86ea2a47bc657f07192366b9ceee3ee0d6
Carlos Alberto Filho <calbertofilho@gmail.com>
2021-11-12 16:37:25 -0300

    Update 12-11-2021

--- components/Header/index.js
+++ components/Header/index.js
@@ -5,37 +5,37 @@ import styles from '../../styles/Home.module.css'
 export default function Footer() {
     return (
         <header className={styles.header}>
-        <div className={styles.logo}>
-        <Link href="#">
-            <a><Image src="/logo.png" alt="Carlos Alberto ETI Logo" width="322" height="94" /></a>
-        </Link>
-        </div>
+          <div className={styles.logo}>
+            <Link href="#">
+              <a><Image src="/logo.png" alt="Carlos Alberto ETI Logo" width="322" height="94" /></a>
+            </Link>
+          </div>
 
-        <div className={styles.menu}>
-        <Link href="http://mail.lberto.eti.br/">
-            <a className={styles.btn}>@</a>
-        </Link>
-        &nbsp;
-        <Link href="#Contato">
-            <a className={styles.btn}>Contato</a>
-        </Link>
-        &nbsp;
-        <Link href="#Sobre">
-            <a className={styles.btn}>Sobre</a>
-        </Link>
-        &nbsp;
-        <Link href="#">
-            <a className={styles.btn}>App</a>
-        </Link>
-        &nbsp;
-        <Link href="#">
-            <a className={styles.btn}>Download</a>
-        </Link>
-        &nbsp;
-        <Link href="#">
-            <a className={styles.btn}>Portfólio</a>
-        </Link>
-        </div>
+          <nav className={styles.menu}>
+            <Link href="http://mail.lberto.eti.br/">
+              <a className={styles.btn}>@</a>
+            </Link>
+            &nbsp;
+            <Link href="#Contato">
+              <a className={styles.btn}>Contato</a>
+            </Link>
+            &nbsp;
+            <Link href="#Sobre">
+              <a className={styles.btn}>Sobre</a>
+            </Link>
+            &nbsp;
+            <Link href="#">
+              <a className={styles.btn}>App</a>
+            </Link>
+            &nbsp;
+            <Link href="#">
+              <a className={styles.btn}>Download</a>
+            </Link>
+            &nbsp;
+            <Link href="#">
+              <a className={styles.btn}>Portfólio</a>
+            </Link>
+          </nav>
         </header>
     );
 }

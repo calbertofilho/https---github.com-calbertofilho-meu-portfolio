1bb3130da5c8ae140848fdf79b770d548137b5e7
Carlos Alberto Filho <calbertofilho@gmail.com>
2021-11-20 12:01:24 -0300

    Update 20-11-2021

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,14 @@
-import Head from 'next/head'
+import Page from '../components/Page'
 import Header from '../components/Header'
 import Main from '../components/Main'
 import Footer from '../components/Footer'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
     return (
-        <div className={styles.container}>
-            <Head>
-                <title>Carlos Alberto ETI</title>
-            </Head>
-
+        <Page title="Carlos Alberto ETI" description="Especialista em Tecnologia da Informação">
             <Header />
             <Main />
             <Footer />
-        </div>
+        </Page>
     )
 }

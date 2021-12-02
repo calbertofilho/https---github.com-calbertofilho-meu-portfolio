8b88826b0fec1f36cb475db9a1f358c4d22de37f
Carlos Alberto Filho <calbertofilho@gmail.com>
2021-12-02 17:16:36 -0300

    Update 02-12-2021

--- others/hexagon-bg/scripts/main.js
+++ others/hexagon-bg/scripts/main.js
@@ -1,7 +1,15 @@
+// Controle do movimento da iluminação no fundo do Background
 const light = document.querySelector(".light");
 const grid = document.querySelector(".hex-grid");
 
 grid.addEventListener("mousemove", function(e) {
 	light.style.left = `${e.clientX}px`;
 	light.style.top = `${e.clientY}px`;
+});
+
+// Controle da emulação de digitação
+var type = new Typed('.typing-text', {
+  strings: [ 'WEB Designer', 'Front-End Developer', 'Graphic Designer' ],
+  typeSpeed: 200,
+  loop: true
 });
